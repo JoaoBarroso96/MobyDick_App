e09eac7cf03a9b9ffd2f2699ee79ef5c665e2fd3
JoaoBarroso96 <joao.barroso.96@gmail.com>
2024-05-27 17:13:51 +0000

    .

--- mobydick/lib/routes/loading/loading.dart
+++ mobydick/lib/routes/loading/loading.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gif/flutter_gif.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:mobydick/services/trips_service.dart';
-import 'package:sn_progress_dialog/sn_progress_dialog.dart';
 
 import '../../mobydick_app_theme.dart';
 
